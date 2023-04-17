f4bf70f085aafc005d4eae2886d88a5d274fa2ea
ysbllcby <ysabellacubay@gmail.com>
2023-04-17 15:32:25 -0600

    Creating a generic class.

---
+++ src/com/linkedin/generics/Fruit.java
@@ -0,0 +1,41 @@
+package com.linkedin.generics;
+
+public class Fruit {
+    private String name;
+    private String countryOfOrigin;
+    private String color;
+    private double weight;
+
+    public Fruit(String name, String countryOfOrigin, String color, double weight) {
+        this.name = name;
+        this.countryOfOrigin = countryOfOrigin;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "name='" + name + '\'' +
+                ", countryOfOrigin='" + countryOfOrigin + '\'' +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
